5d43a2d2add9b1da3e36afa0097a5ce587b44a86
shamanishche <oleg.lenga@gmail.com>
2015-10-27 07:09:47 +0200

    Update project folder.

--- HW2_Matrix/Matrix.Tests/SizeTests.cs
+++ HW2_Matrix/Matrix.Tests/SizeTests.cs
@@ -4,57 +4,57 @@ namespace Matrix.Tests
 {
     public class SizeTests
     {
-        //[Test]
-        //public void WidthProperty_Always_ReturnsWidth()
-        //{
-        //    var size = new Size(width: 1, height: 2);
+        [Test]
+        public void WidthProperty_Always_ReturnsWidth()
+        {
+            var size = new Size(width: 1, height: 2);
 
-        //    Assert.AreEqual(1, size.Width);
-        //}
+            Assert.AreEqual(1, size.Width);
+        }
 
-        //[Test]
-        //public void HeightProperty_Always_ReturnsHeight()
-        //{
-        //    var size = new Size(width: 1, height: 2);
+        [Test]
+        public void HeightProperty_Always_ReturnsHeight()
+        {
+            var size = new Size(width: 1, height: 2);
 
-        //    Assert.AreEqual(2, size.Height);
-        //}
+            Assert.AreEqual(2, size.Height);
+        }
 
-        //[Test]
-        //public void IsSquare_WhenHeightIsTheSameAsWidth_ReturnsTrue()
-        //{
-        //    var size = new Size(width: 1, height: 1);
+        [Test]
+        public void IsSquare_WhenHeightIsTheSameAsWidth_ReturnsTrue()
+        {
+            var size = new Size(width: 1, height: 1);
 
-        //    Assert.IsTrue(size.IsSquare);
-        //}
+            Assert.IsTrue(size.IsSquare);
+        }
 
-        //[Test]
-        //public void IsSquare_WhenHeightAndWidthAreDifferent_ReturnsFalse()
-        //{
-        //    var size = new Size(width: 1, height: 2);
+        [Test]
+        public void IsSquare_WhenHeightAndWidthAreDifferent_ReturnsFalse()
+        {
+            var size = new Size(width: 1, height: 2);
 
-        //    Assert.IsFalse(size.IsSquare);
-        //}
+            Assert.IsFalse(size.IsSquare);
+        }
 
-        //[Test]
-        //public void EqualsOperator_WhenHeightIsTheSameAsWidth_ReturnsTrue()
-        //{
-        //    var sizeA = new Size(1, 1);
-        //    var sizeB = new Size(1, 1);
+        [Test]
+        public void EqualsOperator_WhenHeightIsTheSameAsWidth_ReturnsTrue()
+        {
+            var sizeA = new Size(1, 1);
+            var sizeB = new Size(1, 1);
 
-        //    Assert.IsTrue(sizeA == sizeB);
-        //}
+            Assert.IsTrue(sizeA == sizeB);
+        }
 
-        //[Test]
-        //public void EqualsOperator_WhenHeightAndWidthAreDifferent_ReturnsFalse()
-        //{
-        //    var sizeA = new Size(1, 1);
-        //    var sizeB = new Size(1, 2);
-        //    var sizeC = new Size(2, 1);
+        [Test]
+        public void EqualsOperator_WhenHeightAndWidthAreDifferent_ReturnsFalse()
+        {
+            var sizeA = new Size(1, 1);
+            var sizeB = new Size(1, 2);
+            var sizeC = new Size(2, 1);
 
-        //    Assert.IsFalse(sizeA == sizeB);
-        //    Assert.IsFalse(sizeA == sizeC);
-        //    Assert.IsFalse(sizeB == sizeC);
-        //}
+            Assert.IsFalse(sizeA == sizeB);
+            Assert.IsFalse(sizeA == sizeC);
+            Assert.IsFalse(sizeB == sizeC);
+        }
     }
 }
